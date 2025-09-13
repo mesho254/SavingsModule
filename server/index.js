e671f71d06122c6a0265094462f785731632b6ed
Meshak Otieno <meshakotieno343@gmail.com>
2025-09-13 12:30:17 +0300

    minor fixes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 const cors = require('cors');
+const corsOptions = { origin: "*", credentials: true, optionSuccessStatus: 200 };
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-require('dotenv').config();
 
-const connectDB = require('./config/db');
+
 const authRoutes = require('./routes/auth');
 const goalRoutes = require('./routes/goals');
 const transactionRoutes = require('./routes/transactions');
@@ -14,10 +16,27 @@ const adminRoutes = require('./routes/admin');
 const reconciliationRoutes = require('./routes/reconciliation');
 
 const app = express();
-connectDB();
+dotenv.config();
 
-app.use(cors());
+// Connect to database
+const mongooseOptions={
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+};
+mongoose.connect(process.env.MONGO_URI,
+mongooseOptions,err=>{
+  if(err){
+      console.log(err)
+  }
+  else{
+      console.log("Connected to MongoDB")
+  }
+});
+
+
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(bodyParser.json());
 
 // Swagger configuration
 const swaggerOptions = {
@@ -51,6 +70,17 @@ app.use('/api/transactions', transactionRoutes);
 app.use('/api/analytics', analyticsRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/reconciliation', reconciliationRoutes);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    team_name: "Mesho Devs", dev_team: ["Mesho", "Mesho254"].sort()
+  });
+});
+
+app.use("/*splat", (req, res) => {
+  res.status(500).json({ status: "error", message: "This route does not exist" });
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}. Swagger docs at http://localhost:${PORT}/api-docs`));

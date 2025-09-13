a449d49c9022e9ba9419645715bddae81c268c68
Meshak Otieno <meshakotieno343@gmail.com>
2025-09-13 13:09:15 +0300

    minor fixes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -11,7 +11,7 @@ const register = async (req, res) => {
 
     const user = await User.create({ name, email, password, role });
 
-    if (user && transaction) {
+    if (user) {
       res.status(201).json({
         _id: user._id,
         name: user.name,
